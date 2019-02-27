afbc172c609fc047e74a90d157c66083bf33a430
fusernames <acaroff10@gmail.com>
2019-02-27 02:32:12 +0100

    user images

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -10,6 +10,7 @@ class User extends Component {
     description: '',
     gender: '',
     pictures: [],
+    mainPic: {}
   }
 
   getUser = () => {
@@ -34,9 +35,17 @@ class User extends Component {
     .then(response => {
       if (response.ok) {
         response.json().then(json => {
-          if (json['error'] == null && json['success'] !== this.state.pictures)
-            this.setState({ pictures: json['success'] });
-          else if (json['error'])
+          if (json['error'] == null && json['success'] !== this.state.pictures) {
+            let pictures = json.success;
+            for (var i in pictures) {
+              if (pictures[i].main) {
+                console.log(pictures[i]);
+                this.setState({ mainPic: pictures[i] });
+                console.log(this);
+              }
+            }
+            this.setState({ pictures: pictures });
+          } else if (json['error'])
             notify('error', this.props.locales.idParser(json['error']));
         });
       } else console.error(new Error(response.statusText));
@@ -58,11 +67,10 @@ class User extends Component {
     return (
       <React.Fragment>
         <h4>{this.state.username}</h4>
-        <div className="pictures">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="row">
+          <div className="col xl10 s12">
+            <div className="picture picture-main" style={{backgroundImage: 'url("/pictures/user/' + this.state.mainPic.picture + '")'}}></div>
+          </div>
         </div>
         <h6>Description</h6>
         <p>{this.state.description}</p>
@@ -71,6 +79,12 @@ class User extends Component {
         <a className="waves-effect waves-light btn"><i className="material-icons left">favorite</i>{locale.user.like}</a>
         <a className="waves-effect waves-light btn red right ml-10"><i className="material-icons left">block</i>{locale.user.block}</a>
         <a className="waves-effect waves-light btn red right"><i className="material-icons left">priority_high</i>{locale.user.report}</a>
+        <h6>More pics</h6>
+        {this.state.pictures.map(pic => {
+          return (
+            <div className="picture" style={{backgroundImage: 'url("/pictures/user/' + pic.picture + '")'}}></div>
+          );
+        })}
       </React.Fragment>
     )
   }
