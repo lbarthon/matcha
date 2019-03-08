939c0da1e0103e316e54d51c0bdea01a986663b8
fusernames <acaroff10@gmail.com>
2019-03-08 13:52:10 +0100

    Chat end ?

--- src/components/chat/ChatSide.jsx
+++ src/components/chat/ChatSide.jsx
@@ -19,11 +19,19 @@ class ChatSide extends Component {
   }
 
   render() {
+    const { locale } = this.props.locales;
     return (
       <React.Fragment>
         <ul id="chatside" class="sidenav">
           {this.props.rooms.map(room => {
-            return <li><a class="waves-effect" onClick={() => this.props.changeRoom(room.id)}>{room.user.username}</a></li>
+            return (
+              <li>
+                <a className="waves-effect" onClick={() => this.props.changeRoom(room.id)}>
+                  {room.user.username}
+                  {room.unread > 0 && <span className="new badge red" data-badge-caption={locale.chat.new}>{room.unread}</span>}
+                </a>
+              </li>
+            )
           })}
         </ul>
         <a href="#" data-target="chatside" className="sidenav-trigger"><i className="material-icons">menu</i></a>
