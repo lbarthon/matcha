9c6a480fc0a1781787b15471e432838f86b93df2
lbarthon <lsbarthonet2@gmail.com>
2019-03-08 13:01:00 +0100

    Lots of todo

--- src/components/match/Match.jsx
+++ src/components/match/Match.jsx
@@ -8,7 +8,8 @@ import Map from './Map';
 class Match extends Component {
 
   state = {
-    matchs: []
+    matchs: [],
+    sorted: []
   }
 
   fetchMatchs = () => {
@@ -25,6 +26,35 @@ class Match extends Component {
       }
     })
     .catch(console.error);
+  }
+
+  // Tous ces sorts doivent pouvoir prendre des limites genre dans this.state.limits pour l'âge, la location etc.
+  // On va bien se marrer mon pote
+
+  defaultSort = () => {
+    // Ce sort c'est un mix de location + tags + popularité
+  }
+
+  reverseSort = () => {
+    // Ça reverse le sort pris en param
+    // En gros, si sort par location la plus proche, ça sortira le plus loin en premier.
+    // Juste ça reverse l'array bien sort
+  }
+
+  locationSort = () => {
+    // Faire des calculs avec lat & lng pr sort le plus proche puis le 2e plus proche etc.
+  }
+
+  ageSort = () => {
+    // Calculs sur birthdate
+  }
+
+  popularitySort = () => {
+    // Yes on sort par popularité
+  }
+
+  commonTagsSort = () => {
+    // Sort en fonction du nombre de tags en commun
   }
 
   componentWillMount = () => {
