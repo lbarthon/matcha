ecc3b39d622cb4b93e8dcfe97be3a6c784a3ffaa
lbarthon <lsbarthonet2@gmail.com>
2019-03-08 14:35:21 +0100

    Location sort + age sort + popularity sort (easier) => DONE

--- src/components/match/Match.jsx
+++ src/components/match/Match.jsx
@@ -9,8 +9,61 @@ class Match extends Component {
 
   state = {
     matchs: [],
+    user: [],
     sorted: []
   }
+
+  sorts = [
+    {
+      key: 'matchs.sorts.default',
+      function: this.defaultSort
+    },
+    {
+      key: 'matchs.sorts.location',
+      function: () => {
+        const deg2rad = (deg) => {
+          return deg * (Math.PI/180)
+        }
+
+        const getDistanceFromLatLon = (lat1,lon1,lat2,lon2) => {
+          var dLat = deg2rad(lat2 - lat1);
+          var dLon = deg2rad(lon2 - lon1); 
+          var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2); 
+          var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
+          return c;
+        }
+
+        var user_split = this.state.user.location.split(";");
+        var lat = user_split[0];
+        var lng = user_split[1];
+        this.setState({ sorted: this.state.matchs.sort((a, b) => {
+          var a_split = a.location.split(";");
+          var b_split = b.location.split(";");
+          return getDistanceFromLatLon(lat, lng, a_split[0], a_split[1]) - getDistanceFromLatLon(lat, lng, b_split[0], b_split[1]);
+        })
+      })}
+    },
+    {
+      key: 'matchs.sorts.age',
+      function: () => {
+        this.setState({ sorted: this.state.matchs.sort((a, b) => {
+            var a_split = a.birthdate.split("/");
+            var b_split = b.birthdate.split("/");
+            return Number(a_split[2] + a_split[1] + a_split[0]) - Number(b_split[2] + b_split[1] + b_split[0]);
+        })
+      })}
+    },
+    {
+      key: 'matchs.sorts.popularity',
+      function: () => {
+        this.setState({ sorted: this.state.matchs.sort((a, b) => b.popularity - a.popularity) });
+      }
+    },
+    {
+      key: 'matchs.sorts.tags',
+      function: this.commonTagsSort
+    }
+  ]
 
   fetchMatchs = () => {
     const { locales } = this.props;
@@ -20,7 +73,23 @@ class Match extends Component {
           if (json.error) {
             notify('error', locales.idParser(json.error))
           } else {
-            this.setState({ matchs: json.success }, this.popularitySort);
+            this.setState({ matchs: json.success });
+          }
+        }).catch(console.error);
+      }
+    })
+    .catch(console.error);
+  }
+
+  fetchUser = () => {
+    const { locales } = this.props;
+    fetch("/api/user/current").then(response => {
+      if (response.ok) {
+        response.json().then(json => {
+          if (json.error) {
+            notify('error', locales.idParser(json.error))
+          } else {
+            this.setState({ user: json.success });
           }
         }).catch(console.error);
       }
@@ -36,7 +105,6 @@ class Match extends Component {
   }
 
   reverseSort = () => {
-    this.setState({ sorted: this.state.sorted.reverse() });
     // Ça reverse le sort pris en param
     // En gros, si sort par location la plus proche, ça sortira le plus loin en premier.
     // Juste ça reverse l'array bien sort
@@ -50,16 +118,12 @@ class Match extends Component {
     // Calculs sur birthdate
   }
 
-  popularitySort = () => {
-    this.setState({ sorted: this.state.matchs.sort((a, b) => a.popularity - b.popularity) });
-    // Popularité reverse
-  }
-
   commonTagsSort = () => {
     // Sort en fonction du nombre de tags en commun
   }
 
   componentWillMount = () => {
+    this.fetchUser();
     this.fetchMatchs();
   }
 
@@ -70,7 +134,10 @@ class Match extends Component {
 
     return (
       <div>
-        <Map matchs={matchs} userLocation={"30;30"} />
+        <Map matchs={matchs} userLocation={this.state.user.location} />
+        {this.sorts.map(value => {
+            return (<button onClick={value.function}>{value.key}</button>)
+        })}
         {sorted.map(value => {
           return (
             <div>
